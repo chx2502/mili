5494c866237964a07176e685da9fb40f0a341c98
chx <chx2502@gmail.com>
2020-10-18 15:47:56 +0800

    修改Nginx配置

--- docker/build.sh
+++ docker/build.sh
@@ -3,5 +3,5 @@ npm run build
 cp -r ../dist ./
 # 将镜像推送到Registry
 docker login --username=我给你打折吧 registry.cn-shanghai.aliyuncs.com
-docker build -t registry.cn-shanghai.aliyuncs.com/chx2502/mili:v1.0.0 ./
-docker push registry.cn-shanghai.aliyuncs.com/chx2502/mili:v1.0.0
+docker build -t registry.cn-shanghai.aliyuncs.com/chx2502/mili:v1.0.x ./
+docker push registry.cn-shanghai.aliyuncs.com/chx2502/mili:v1.0.x
